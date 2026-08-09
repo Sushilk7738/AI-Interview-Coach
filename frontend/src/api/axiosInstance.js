04e406d5e3fd6713ca6ad7dc70340cf45792281e
Sushilk <sushilkamble7738@gmail.com>
2026-08-09 12:06:32 +0530

    fix: hardcode production API URL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,75 +1,55 @@
 import axios from 'axios';
 import { getAccessToken, getRefreshToken, saveTokens, removeTokens } from '../utils/token';
-import { refreshAccessToken } from "./authApi";
+import { refreshAccessToken } from './authApi';
 
 const api = axios.create({
-    baseURL : "http://127.0.0.1:8000/api/",
+    baseURL: 'https://ai-interview-coach-backend-lz1i.onrender.com/api/',
 });
-
-const refreshApi = axios.create({
-    baseURL : "http://127.0.0.1:8000/api/",
-});
-
 
 api.interceptors.request.use(
-    (config) => {
-        const token = getAccessToken();
+(config) => {
+    const token = getAccessToken();
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-
-    (error) =>{
-        return Promise.reject(error);
+    if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
+},
+(error) => Promise.reject(error)
 );
 
-
-
 api.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        
-        const originalRequest = error.config;
+(response) => response,
+async (error) => {
+    const originalRequest = error.config;
 
-        if(
-            error.response?.status === 401 &&
-            !originalRequest._retry
-        ) {
-            originalRequest._retry = true;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
 
-            try {
+    try {
+        const refresh = getRefreshToken();
 
-                const refresh = getRefreshToken();
-
-                if (!refresh) {
-                    removeTokens();
-                    return Promise.reject(error);
-                }
-
-                const data = await refreshAccessToken(refresh);
-
-                saveTokens(data.access, refresh);
-
-                originalRequest.headers.Authorization = `Bearer ${data.access}`;
-
-                return api(originalRequest);
-
-            } catch (err) {
-
-                removeTokens();
-
-                return Promise.reject(err);
-
-            }
-
+        if (!refresh) {
+        removeTokens();
+        return Promise.reject(error);
         }
 
-        return Promise.reject(error);
+        const data = await refreshAccessToken(refresh);
+
+        saveTokens(data.access, refresh);
+
+        originalRequest.headers.Authorization = `Bearer ${data.access}`;
+
+        return api(originalRequest);
+    } catch (err) {
+        removeTokens();
+        return Promise.reject(err);
     }
+    }
+
+    return Promise.reject(error);
+}
 );
 
 export default api;
